c3ac1af4919c1e532e416cc6e7331c38f332260e
fredrickvaagen <fredrick.vaagen@fjordline.com>
2024-10-23 13:17:19 +0200

    adjust tabell

--- src/app/(main)/turnering/tabell/ResultsTable.tsx
+++ src/app/(main)/turnering/tabell/ResultsTable.tsx
@@ -28,52 +28,79 @@ type TeamStats = {
   image: string
 }
 
-interface ResultsTableProps {
+
+
+
+interface ResultsTableProps
+{
   matchResults: MatchResult[]
   sanityTeamData: MythicPlusTeam[]
 }
 
-const ResultsTable: React.FC<ResultsTableProps> = ({ matchResults, sanityTeamData }) => {
-  // Calculate team stats
-  const calculateTeamStats = (results: MatchResult[]): TeamStats[] => {
+const ResultsTable: React.FC<ResultsTableProps> = ({ matchResults, sanityTeamData }) =>
+{
+
+
+  // Calculate team stats based on match results
+  const calculateTeamStats = (results: MatchResult[]): TeamStats[] =>
+  {
     const teamStatsMap: Record<string, TeamStats> = {}
 
-    results.forEach((result) => {
-      if (!teamStatsMap[result.team_slug]) {
-        teamStatsMap[result.team_slug] = {
-          team_slug: result.team_slug,
-          team_name: sanityTeamData.find((team) => team.teamSlug === result.team_slug)?.teamName ?? '',
+    // Preprocess sanityTeamData for faster lookup
+    const teamDataMap: Record<string, MythicPlusTeam> = {}
+    sanityTeamData.forEach(team =>
+    {
+      teamDataMap[ team.teamSlug ] = team
+    })
+
+    results.forEach((result) =>
+    {
+      const teamSlug = result.team_slug
+
+      // Initialize team stats if not present
+      if (!teamStatsMap[ teamSlug ]) {
+        const teamData = teamDataMap[ teamSlug ]
+        teamStatsMap[ teamSlug ] = {
+          team_slug: teamSlug,
+          team_name: teamData?.teamName ?? '',
           wins: 0,
           losses: 0,
           points: 0,
-          image: sanityTeamData.find((team) => team.teamSlug === result.team_slug)?.teamImage.asset._ref ?? '',
+          image: teamData?.teamImage.asset._ref ?? '',
         }
       }
 
-      const teamStats = teamStatsMap[result.team_slug]
-      let points = 0
-      if (result.match_1 === 1 && result.match_2 === 1) {
-        points = 2 // Win both matches
-      } else if (
-        (result.match_1 === 1 && result.match_2 === 0 && result.match_3 === 1) ||
-        (result.match_1 === 0 && result.match_2 === 1 && result.match_3 === 1)
-      ) {
-        points = 1 // Win one and win tie-breaker
-      }
+      const teamStats = teamStatsMap[ teamSlug ]
 
-      if (result.winner) {
+      // Calculate the number of matches won by the team
+      const matchesWon = [ result.match_1, result.match_2, result.match_3 ]
+        .filter((match) => match === 1).length
+
+      // Determine if match_3 was played
+      const match3Played = result.match_3 === 0 || result.match_3 === 1
+
+      if (matchesWon >= 2) {
+        // Team wins overall
+        teamStats.points += 2
         teamStats.wins += 1
+      } else if (match3Played) {
+        // Team loses overall but match_3 was played
+        teamStats.points += 1
+        teamStats.losses += 1
       } else {
+        // Team loses overall and match_3 was not played
+        teamStats.points += 0
         teamStats.losses += 1
       }
-
-      teamStats.points += points
     })
 
     return Object.values(teamStatsMap)
   }
 
   const teamStats = calculateTeamStats(matchResults)
+
+  const sortedTeamStats = [ ...teamStats ].sort((a, b) => b.points - a.points);
+
 
   return (
     <div className="p-4 mt-4">
@@ -87,29 +114,27 @@ const ResultsTable: React.FC<ResultsTableProps> = ({ matchResults, sanityTeamDat
           </TableRow>
         </TableHeader>
         <TableBody className="rounded-b-lg">
-          {teamStats.map((team, index) => {
-            return (
-              <TableRow key={index} className="bg-[#052D49] even:bg-[#0B436C] border-none text-white">
-                <TableCell className="min-w-32 text-white font-bold flex items-center border-r-[1px] border-black">
-                  <Image
-                    src={urlForImage(team.image)}
-                    alt={`${team.team_name} logo`}
-                    width={45}
-                    height={45}
-                    className="mr-3 w-8 h-8 rounded-full"
-                  />
-                  <span className="text-sm font-bold"> {team.team_name}</span>
-                </TableCell>
-                <TableCell className="font-bold text-[#FCD20A] text-lg text-center border-r-[1px] border-black">
-                  {team.wins}
-                </TableCell>
-                <TableCell className="font-bold text-[#FCD20A] text-lg text-center border-r-[1px] border-black">
-                  {team.losses}
-                </TableCell>
-                <TableCell className="font-bold text-[#FCD20A] text-lg text-center">{team.points}</TableCell>
-              </TableRow>
-            )
-          })}
+          {sortedTeamStats.map((team, index) => (
+            <TableRow key={index} className="bg-[#052D49] even:bg-[#0B436C] border-none text-white">
+              <TableCell className="min-w-32 text-white font-bold flex items-center border-r-[1px] border-black">
+                <Image
+                  src={urlForImage(team.image)}
+                  alt={`${team.team_name} logo`}
+                  width={45}
+                  height={45}
+                  className="mr-3 w-8 h-8 rounded-full"
+                />
+                <span className="text-sm font-bold">{team.team_name}</span>
+              </TableCell>
+              <TableCell className="font-bold text-[#FCD20A] text-lg text-center border-r-[1px] border-black">
+                {team.wins}
+              </TableCell>
+              <TableCell className="font-bold text-[#FCD20A] text-lg text-center border-r-[1px] border-black">
+                {team.losses}
+              </TableCell>
+              <TableCell className="font-bold text-[#FCD20A] text-lg text-center">{team.points}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
@@ -117,3 +142,6 @@ const ResultsTable: React.FC<ResultsTableProps> = ({ matchResults, sanityTeamDat
 }
 
 export default ResultsTable
+
+
+
